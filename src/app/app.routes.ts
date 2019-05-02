09a9403134478168650e5edeae90458f4e554ea1
Noah Osterhout <noahosterhout@Noahs-MacBook-Pro.local>
2019-05-02 18:18:28 -0400

    Added path and route instructions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 
+// When adding to the routes the path name has to equal the name of the component!
+// IE: name.component is the component but the path is 'name'
 export const routes: Routes = [
   {
       path: '',
