157b2a26a2ce2b47459a4b55a4bc35f69a72c667
NoahFlowa <osterhoutn@gmail.com>
2019-05-02 20:04:27 -0400

    Added components to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './Onboarding/signup/signup.component';
+import { LoginComponent } from './Onboarding/login/login.component';
+import { ForgotComponent } from './Onboarding/forgot/forgot.component';
+import { NewUserComponent } from './Onboarding/new-user/new-user.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { ErrorComponent } from './Shared/error/error.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { SettingsComponent } from './settings/settings.component';
+import { UpdatesComponent } from './Shared/updates/updates.component';
 
 // When adding to the routes the path name has to equal the name of the component!
 // IE: name.component is the component but the path is 'name'
@@ -13,5 +24,49 @@ export const routes: Routes = [
   {
       path: 'home',
       component: HomeComponent,
+  },
+  {
+      path: 'signup',
+      component: SignupComponent,
+  },
+  {
+      path: 'login',
+      component: LoginComponent,
+  },
+  {
+      path: 'forgot',
+      component: ForgotComponent,
+  },
+  {
+      path: 'new-user',
+      component: NewUserComponent,
+  },
+  {
+      path: 'footer',
+      component: FooterComponent,
+  },
+  {
+      path: 'navbar',
+      component: NavbarComponent,
+  },
+  {
+      path: 'error-screen',
+      component: ErrorComponent,
+  },
+  {
+      path: 'messages',
+      component: MessagesComponent,
+  },
+  {
+      path: 'rooms',
+      component: RoomsComponent,
+  },
+  {
+      path: 'settings',
+      component: SettingsComponent,
+  },
+  {
+      path: 'updates',
+      component: UpdatesComponent,
   },
 ];
